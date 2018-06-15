e89f2843a22da0b0a42aaf07e47b5b46e6859121
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-15 23:51:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,8 @@
 # Your code goes here!
 class Dog
-      def initialize(name)
-            @name = name
-      end
+      # def initialize(name)
+      #       @name = name
+      # end
       def name= (name)
             @name = name
       end
